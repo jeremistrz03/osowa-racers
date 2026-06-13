7ee2989d06485c30295a97235615f1ab16e345c1
jodla1337 <herbatka500@gmail.com>
2026-06-13 14:13:15 +0200

    fix konfliktow

--- car2.gd
+++ car2.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,9 +11,9 @@ var acceleration = 5
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left") and not speed == 0:
-		rotation -= deg_to_rad(2/int(pow(speed, 0.15)))
+		rotation -= deg_to_rad(2/pow(speed, 0.15))
 	if Input.is_action_pressed("ui_right") and not speed == 0:
-		rotation += deg_to_rad(2/int(pow(speed, 0.15)))
+		rotation += deg_to_rad(2/pow(speed, 0.15))
 		
 	if Input.is_action_pressed("ui_up"):
 		speed = min(500, speed + acceleration)
@@ -23,6 +23,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_down"):
 		speed = max(0, speed - acceleration * 2)
 		
-	var vector = Vector2.RIGHT.rotated(rotation) * speed
+	var vector = Vector2.UP.rotated(rotation) * speed
 	
 	position += vector * delta
